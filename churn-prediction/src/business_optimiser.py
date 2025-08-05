93298348659026f7856990c4075da2f313cd3585
Muhammadali1029 <104990048+Muhammadali1029@users.noreply.github.com>
2025-08-05 22:48:00 +0100

    Run Business optimiser report and plots in notebook

--- churn-prediction/src/business_optimiser.py
+++ churn-prediction/src/business_optimiser.py
@@ -8,7 +8,7 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-from logger_config import setup_logger
+from .logger_config import setup_logger
 
 logger = setup_logger(__name__)
 
